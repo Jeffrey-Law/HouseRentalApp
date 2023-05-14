bcea09123044bc2baed5af2ccd36046f35c3da26
joelui001 <109941208+joelui001@users.noreply.github.com>
2023-05-14 14:56:04 +0800

    add posting impl

--- app/src/main/java/com/example/login/PostingActivity.java
+++ app/src/main/java/com/example/login/PostingActivity.java
@@ -3,17 +3,121 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PostingActivity extends AppCompatActivity {
+import javax.net.ssl.HostnameVerifier;
+
+public class PostingActivity extends AppCompatActivity implements
+AdapterView.OnItemSelectedListener{
+
+    String[] options = {"Please Select","Yes", "No"};
+    String[] houseTypes = {"House", "Apartment & unit", "Townhouse", "Villa", "Others"};
+
+    String[] publishTime = {"7 Days", "30 Days", "90 Days", "180 Days", "365 Days"};
+    Spinner furnished, petConsidered, visibility, houseType, publishingTime;
+
+    TextView price, address, numOfBedroom, numOfCarSpace, description;
+    Button upLoadPhotoBtn = findViewById(R.id.uploadPhoto_btn);
+    Button postingBtn = findViewById(R.id.posting_btn);
+
+    AppDatabase appDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting);
 
+        appDatabase = MainActivity.getAppDatabase();
+
+        price = findViewById(R.id.price_tv);
+        address = findViewById(R.id.address_tv);
+        numOfBedroom = findViewById(R.id.bedroom_tv);
+        numOfCarSpace = findViewById(R.id.carspace_tv);
+
+        furnished = findViewById(R.id.furnished_spinner);
+        petConsidered = findViewById(R.id.pet_spinner);
+        visibility = findViewById(R.id.visibility_spinner);
+        houseType = findViewById(R.id.housetype_spinner);
+        publishingTime = findViewById(R.id.publishing_time_spinner);
+
+        ArrayAdapter<String> optionsAd  = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, options);
+        optionsAd.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> houseTypesAd  = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, houseTypes);
+        houseTypesAd.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> publishTimeAd  = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, publishTime);
+        publishTimeAd.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        furnished.setAdapter(optionsAd);
+        furnished.setOnItemSelectedListener(this);
+
+        petConsidered.setAdapter(optionsAd);
+        petConsidered.setOnItemSelectedListener(this);
+
+        visibility.setAdapter(optionsAd);
+        visibility.setOnItemSelectedListener(this);
+
+        houseType.setAdapter(houseTypesAd);
+        houseType.setOnItemSelectedListener(this);
+
+        //upLoadPhotoBtn.setOnClickListener();
+        postingBtn.setOnClickListener(posting);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    private View.OnClickListener posting = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            String price_txt = price.getText().toString();
+            String address_txt = address.getText().toString();
+            String numOfBedroom_txt = numOfBedroom.getText().toString();
+            String numOfCarSpace_txt = numOfBedroom.getText().toString();
+            int furnished_sel = furnished.getSelectedItemPosition();
+            int petConsidered_sel = petConsidered.getSelectedItemPosition();
+            String houseType_sel = houseType.getSelectedItem().toString();
+
+            Context context = getApplicationContext();
+            if(price_txt.length()==0 || address_txt.length()==0 || numOfBedroom_txt.length()==0 ||
+            numOfCarSpace_txt.length()==0 || furnished_sel == 0 || petConsidered_sel == 0){
+                CharSequence text = "Please fill in all the necessary data";
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+            }else{
+                House house = new House();
+                CharSequence text = "Posted";
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    };
+}
+
+
+
 
 //    private void loadImagesFromGallery() {
 //
@@ -81,4 +185,3 @@ public class PostingActivity extends AppCompatActivity {
 //        }
 //
 //    }
-}
