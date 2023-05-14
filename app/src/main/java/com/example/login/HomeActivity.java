e299ac2b3624c3dd78eb60943f7408b346723da4
Jeffrey-Law <lawlaiyin813@yahoo.com.hk>
2023-05-15 00:38:41 +0800

    Update HomeActivity
    Fixed homeStoryBug;
    Added 3 more story in Home;
    Initialized bottom navigation in HomeActivity;
    Changed the drawable used in HomeActivity;

--- app/src/main/java/com/example/login/HomeActivity.java
+++ app/src/main/java/com/example/login/HomeActivity.java
@@ -1,22 +1,37 @@
 package com.example.login;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 public class HomeActivity extends AppCompatActivity {
     private ImageButton ib_favourite;
     private Button btn_searchwithmap, btn_search;
-    private CardView outer_ad_1, outer_ad_2;
-    private ImageView ad_1_iv, ad_2_iv;
+    private CardView outer_ad_1, outer_ad_2, outer_ad_3, outer_ad_4, outer_ad_5;
+    private ImageView ad_1_iv, ad_2_iv, ad_3_iv, ad_4_iv, ad_5_iv;
     private EditText et_search_box;
+    private LinearLayout homeBtn, profileBtn, postBtn, notificationBtn, settingBtn;
+    private int[] adbgColor = new int[5];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +43,54 @@ public class HomeActivity extends AppCompatActivity {
         btn_search = findViewById(R.id.btn_search);
         outer_ad_1 = findViewById(R.id.outer_ad_1);
         outer_ad_2 = findViewById(R.id.outer_ad_2);
+        outer_ad_3 = findViewById(R.id.outer_ad_3);
+        outer_ad_4 = findViewById(R.id.outer_ad_4);
+        outer_ad_5 = findViewById(R.id.outer_ad_5);
         ad_1_iv = findViewById(R.id.ad_1_iv);
         ad_2_iv = findViewById(R.id.ad_2_iv);
+        ad_3_iv = findViewById(R.id.ad_3_iv);
+        ad_4_iv = findViewById(R.id.ad_4_iv);
+        ad_5_iv = findViewById(R.id.ad_5_iv);
         et_search_box = findViewById(R.id.et_search_box);
+        homeBtn = findViewById(R.id.homeBtn);
+        profileBtn = findViewById(R.id.profileBtn);
+        postBtn = findViewById(R.id.postBtn);
+        notificationBtn = findViewById(R.id.notificationBtn);
+        settingBtn = findViewById(R.id.settingBtn);
+
+        // Get default adbgColor
+        adbgColor[0] = outer_ad_1.getCardBackgroundColor().getDefaultColor();
+        adbgColor[1] = outer_ad_2.getCardBackgroundColor().getDefaultColor();
+        adbgColor[2] = outer_ad_3.getCardBackgroundColor().getDefaultColor();
+        adbgColor[3] = outer_ad_4.getCardBackgroundColor().getDefaultColor();
+        adbgColor[4] = outer_ad_5.getCardBackgroundColor().getDefaultColor();
 
         ib_favourite.setOnClickListener(ib_favourite_listener);
         btn_searchwithmap.setOnClickListener(btn_searchwithmap_listener);
         btn_search.setOnClickListener(btn_search_listener);
         ad_1_iv.setOnClickListener(ad_1_iv_listener);
         ad_2_iv.setOnClickListener(ad_2_iv_listener);
+        ad_3_iv.setOnClickListener(ad_3_iv_listener);
+        ad_4_iv.setOnClickListener(ad_4_iv_listener);
+        ad_5_iv.setOnClickListener(ad_5_iv_listener);
+        homeBtn.setOnClickListener(homeBtn_listener);
+        profileBtn.setOnClickListener(profileBtn_listener);
+        postBtn.setOnClickListener(postBtn_listener);
+        notificationBtn.setOnClickListener(notificationBtn_listener);
+        settingBtn.setOnClickListener(settingBtn_listener);
+
+        // Will be laggy if not using glide to load images
+        glideImage(R.drawable.ad1_1, ad_1_iv);
+        glideImage(R.drawable.ad2_1, ad_2_iv);
+            glideImage(R.drawable.ad3_1, ad_3_iv);
+            glideImage(R.drawable.ad4_1, ad_4_iv);
+        glideImage(R.drawable.ad5_1, ad_5_iv);
     }
 
     private View.OnClickListener ib_favourite_listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(HomeActivity.this, PostingActivity.class);
+            Intent intent = new Intent(HomeActivity.this, favourite.class);
             startActivity(intent);
         }
     };
@@ -50,7 +98,7 @@ public class HomeActivity extends AppCompatActivity {
     private View.OnClickListener btn_searchwithmap_listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(HomeActivity.this, PostingActivity.class);
+            Intent intent = new Intent(HomeActivity.this, PostingActivity.class); // TODO
             startActivity(intent);
         }
     };
@@ -58,7 +106,48 @@ public class HomeActivity extends AppCompatActivity {
     private View.OnClickListener btn_search_listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, search_result.class);
+            // TODO: Pass edittext content to search
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener homeBtn_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener profileBtn_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, profile.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener postBtn_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
             Intent intent = new Intent(HomeActivity.this, PostingActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener notificationBtn_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, PostingActivity.class); // TODO
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener settingBtn_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, PostingActivity.class); // TODO
             startActivity(intent);
         }
     };
@@ -68,6 +157,7 @@ public class HomeActivity extends AppCompatActivity {
         public void onClick(View v) {
             Intent intent = new Intent(HomeActivity.this, homeStoryActivity.class);
             intent.putExtra("target", "ad_1");
+            intent.putExtra("adbgColor", adbgColor);
             startActivity(intent);
         }
     };
@@ -77,7 +167,77 @@ public class HomeActivity extends AppCompatActivity {
         public void onClick(View v) {
             Intent intent = new Intent(HomeActivity.this, homeStoryActivity.class);
             intent.putExtra("target", "ad_2");
+            intent.putExtra("adbgColor", adbgColor);
             startActivity(intent);
         }
     };
+
+    private View.OnClickListener ad_3_iv_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, homeStoryActivity.class);
+            intent.putExtra("target", "ad_3");
+            intent.putExtra("adbgColor", adbgColor);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener ad_4_iv_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, homeStoryActivity.class);
+            intent.putExtra("target", "ad_4");
+            intent.putExtra("adbgColor", adbgColor);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener ad_5_iv_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, homeStoryActivity.class);
+            intent.putExtra("target", "ad_5");
+            intent.putExtra("adbgColor", adbgColor);
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Get adbgColor from homeStoryActivity
+        Intent intent = getIntent();
+        if (intent != null) {
+            int[] updatedAdbgColor = intent.getIntArrayExtra("adbgColor");
+            if (updatedAdbgColor != null) {
+                adbgColor = updatedAdbgColor;
+            }
+        }
+        // Set adbgColor
+        outer_ad_1.setCardBackgroundColor(adbgColor[0]);
+        outer_ad_2.setCardBackgroundColor(adbgColor[1]);
+        outer_ad_3.setCardBackgroundColor(adbgColor[2]);
+        outer_ad_4.setCardBackgroundColor(adbgColor[3]);
+        outer_ad_5.setCardBackgroundColor(adbgColor[4]);
+    }
+
+    private void glideImage(int image, ImageView imageView) {
+        Glide.with(this)
+                .load(image)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        Toast.makeText(HomeActivity.this, "Failed to load image.", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        return false;
+                    }
+                })
+                .into(imageView);
+    }
 }
+
--- app/src/main/java/com/example/login/homeStoryActivity.java
+++ app/src/main/java/com/example/login/homeStoryActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -19,14 +20,15 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 
 import jp.shts.android.storiesprogressview.StoriesProgressView;
-
+// Reference: https://blog.devgenius.io/creating-instagram-story-view-in-android-b5e4dbb27185
 public class homeStoryActivity extends AppCompatActivity implements StoriesProgressView.StoriesListener {
 
-    private final int[] images = {R.drawable.ad1_1, R.drawable.ad1_2, R.drawable.ad1_3, R.drawable.ad2_1, R.drawable.ad2_2,R.drawable.ad2_3, R.drawable.ad3_1, R.drawable.ad3_2, R.drawable.ad3_3};
-    private final String[] titles = {"On Sale!", "On Sale!", "On Sale!", "Close To MTR!", "Close To MTR!", "Close To MTR!", "Rural Recommendation!", "Rural Recommendation!", "Rural Recommendation!"};
-    private final String[] postTimes = {"Posted on 11/05/2023", "Posted on 11/05/2023", "Posted on 11/05/2023","Posted on 14/05/2023", "Posted on 14/05/2023", "Posted on 14/05/2023", "Posted on 15/05/2023", "Posted on 15/05/2023", "Posted on 15/05/2023"};
-    private final String[] captions = {"Discount up to 50% off!", "Discount up to 50% off!", "Discount up to 50% off!", "2 minutes walk to MTR station!", "2 minutes walk to MTR station!", "2 minutes walk to MTR station!", "Stay away from crowded city!", "Stay away from crowded city!", "Stay away from crowded city!"};
+    private final int[] images = {R.drawable.ad1_1, R.drawable.ad1_2, R.drawable.ad1_3, R.drawable.ad2_1, R.drawable.ad2_2,R.drawable.ad2_3, R.drawable.ad3_1, R.drawable.ad3_2, R.drawable.ad3_3, R.drawable.ad4_1, R.drawable.ad4_2, R.drawable.ad4_3, R.drawable.ad5_1, R.drawable.ad5_2, R.drawable.ad5_3};
+    private final String[] titles = {"On Sale!", "On Sale!", "On Sale!", "Close To MTR!", "Close To MTR!", "Close To MTR!", "Rural Recommendation!", "Rural Recommendation!", "Rural Recommendation!", "White Houses", "White Houses","White Houses","Nature","Nature","Nature",};
+    private final String[] postTimes = {"Posted on 02/05/2023", "Posted on 02/05/2023", "Posted on 02/05/2023","Posted on 05/05/2023", "Posted on 05/05/2023", "Posted on 05/05/2023","Posted on 09/05/2023", "Posted on 09/05/2023", "Posted on 09/05/2023","Posted on 14/05/2023", "Posted on 14/05/2023", "Posted on 14/05/2023", "Posted on 15/05/2023", "Posted on 15/05/2023", "Posted on 15/05/2023"};
+    private final String[] captions = {"Discount up to 50% off!", "Discount up to 50% off!", "Discount up to 50% off!", "2 minutes walk to MTR station!", "2 minutes walk to MTR station!", "2 minutes walk to MTR station!", "Stay away from crowded city!", "Stay away from crowded city!", "Stay away from crowded city!", "Wanna stay in a plain white house?", "Wanna stay in a plain white house?", "Wanna stay in a plain white house?", "Imagine living with nature and wild animals!", "Imagine living with nature and wild animals!", "Imagine living with nature and wild animals!"};
 
+    private int[] adbgColor = new int[5];
     long pressTime = 0L;
     long limit = 500L;
 
@@ -39,17 +41,28 @@ public class homeStoryActivity extends AppCompatActivity implements StoriesProgr
 
     private int counter = 0;
     private int countBase = 0;
+    private Intent i;
 
-    private int setCounter(String adNum)
-    {
-        if(adNum == "ad_1")
-            return 0;
+    private int setCounter(String adNum) {
+        if (adNum.equals("ad_1"))
+        {adbgColor[0] = R.color.light_grey;
+            return 0;}
 
-        if(adNum == "ad_2")
-            return 3;
+        if (adNum.equals("ad_2"))
+        {adbgColor[1] = R.color.light_grey;
+            return 3;}
 
-        if(adNum == "ad_3")
-            return 6;
+        if (adNum.equals("ad_3"))
+        {adbgColor[2] = R.color.light_grey;
+            return 6;}
+
+        if (adNum.equals("ad_4"))
+        {adbgColor[3] = R.color.light_grey;
+            return 9;}
+
+        if (adNum.equals("ad_5"))
+        {adbgColor[4] = R.color.light_grey;
+            return 12;}
 
         return 0;
     }
@@ -59,24 +72,12 @@ public class homeStoryActivity extends AppCompatActivity implements StoriesProgr
         public boolean onTouch(View view, MotionEvent motionEvent) {
             switch (motionEvent.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-
-                    // on action down when we press our screen
-                    // the story will pause for specific time.
                     pressTime = System.currentTimeMillis();
-
-                    // on below line we are pausing our indicator.
                     storiesProgressView.pause();
                     return false;
                 case MotionEvent.ACTION_UP:
-
-                    // in action up case when user do not touches
-                    // screen this method will skip to next image.
                     long now = System.currentTimeMillis();
-
-                    // on below line we are resuming our progress bar for status.
                     storiesProgressView.resume();
-
-                    // on below line we are returning if the limit < now - presstime
                     return limit < now - pressTime;
             }
             return false;
@@ -89,42 +90,32 @@ public class homeStoryActivity extends AppCompatActivity implements StoriesProgr
         setContentView(R.layout.activity_home_story);
 
         // Set counter to appropriate value based on ad number
-        Bundle adNum = getIntent().getExtras();
-        if(adNum != null)
-            countBase = setCounter(adNum.getString("target"));
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null)
+        {
+            int[] updatedAdbgColor = bundle.getIntArray("adbgColor");
+            if(updatedAdbgColor != null)
+                adbgColor = updatedAdbgColor;
 
-        counter = countBase;
+            countBase = setCounter(bundle.getString("target"));
+            counter = countBase;
+        }
 
         storiesProgressView = (StoriesProgressView) findViewById(R.id.stories);
-
-        // on below line we are setting the total count for our stories.
-        storiesProgressView.setStoriesCount(images.length);
-
-        // on below line we are setting story duration for each story.
+        storiesProgressView.setStoriesCount(3); // Fixed at 3 images for all stories
         storiesProgressView.setStoryDuration(3000L);
-
-        // on below line we are calling a method for set
-        // on story listener and passing context to it.
         storiesProgressView.setStoriesListener(this);
+        storiesProgressView.startStories(0);
 
-        // below line is use to start stories progress bar.
-        storiesProgressView.startStories(counter);
-
-        // initializing our image view.
         image = (ImageView) findViewById(R.id.image);
-
         title_tv = findViewById(R.id.title_tv);
         postTime_tv = findViewById(R.id.postTime_tv);
         caption_tv = findViewById(R.id.caption_tv);
 
-        // on below line we are setting image to our image view.
+        // Using glide to load image to imageview
         glideImage(images[counter],titles[counter],postTimes[counter],captions[counter]);
 
-        // below is the view for going to the previous story.
-        // initializing our previous view.
         View previous = findViewById(R.id.previous);
-
-        // adding on click listener for our reverse view.
         previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -133,13 +124,8 @@ public class homeStoryActivity extends AppCompatActivity implements StoriesProgr
                 storiesProgressView.reverse();
             }
         });
-
-        // on below line we are calling a set on touch
-        // listener method to move towards previous image.
         previous.setOnTouchListener(onTouchListener);
 
-        // on below line we are initializing
-        // view to skip a specific story.
         View next = findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -149,41 +135,29 @@ public class homeStoryActivity extends AppCompatActivity implements StoriesProgr
                 storiesProgressView.skip();
             }
         });
-        // on below line we are calling a set on touch
-        // listener method to move to next story.
         next.setOnTouchListener(onTouchListener);
     }
     @Override
     public void onNext() {
-        // this method is called when we move
-        // to next progress view of story.
         glideImage(images[++counter],titles[counter],postTimes[counter],captions[counter]);
     }
 
     @Override
     public void onPrev() {
-
-        // this method id called when we move to previous story.
-        // on below line we are decreasing our counter
         if ((counter - 1) < countBase) return;
         glideImage(images[--counter],titles[counter],postTimes[counter],captions[counter]);
-
-        // on below line we are setting image to image view
     }
 
     @Override
     public void onComplete() {
-        // when the stories are completed this method is called.
-        // in this method we are moving back to initial main activity.
         Intent i = new Intent(homeStoryActivity.this, HomeActivity.class);
+        i.putExtra("adbgColor", adbgColor);
         startActivity(i);
         finish();
     }
 
     @Override
     protected void onDestroy() {
-        // in on destroy method we are destroying
-        // our stories progress view.
         storiesProgressView.destroy();
         super.onDestroy();
     }
@@ -209,6 +183,5 @@ public class homeStoryActivity extends AppCompatActivity implements StoriesProgr
         title_tv.setText(title);
         postTime_tv.setText(postTime);
         caption_tv.setText(caption);
-
     }
 }
